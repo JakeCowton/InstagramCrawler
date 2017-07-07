7babe37d9a670ce3cdd760e62360ce86eda0d43a
Antonie Lin <iammrhelo@gmail.com>
2017-07-07 20:28:17 +0800

    uncomment

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ export PATH=$PATH:$(pwd)
 
 echo "Check photos & captions..."
 echo "Download first 10 photos and captions of account 'instagram'"
-#python instagramcrawler.py -q 'instagram' -n 20 -c
+python instagramcrawler.py -q 'instagram' -n 20 -c
 
 echo "Check hashtag..."
 echo "Search for hashtag '#breakfast' and download 20 photos"
-#python instagramcrawler.py -q '#breakfast' -n 20
+python instagramcrawler.py -q '#breakfast' -n 20
